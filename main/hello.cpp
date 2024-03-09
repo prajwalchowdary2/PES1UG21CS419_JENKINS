3fe311d7310faf9cd0f873d741437d5b37a2993f
prajwalchowdary2 <103565130+prajwalchowdary2@users.noreply.github.com>
2024-03-09 22:58:14 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG21CS419";
   cout << "I have successfully built and run\n";
+  cout << "PES1UG21CS419 I HAVE SUCEFULLY BUILT AND RUN\n";
   return 0;
 }
